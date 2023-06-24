e75fbcf349bb3befbcbe11acf613a4c98698aa8c
saurabh kedar <saurabhkedar@gmail.com>
2023-06-24 21:45:16 +0530

    added the react component

--- App.py
+++ App.py
@@ -1,47 +1,48 @@
 import streamlit as st
 
+
 def main():
     # Set the background color
     page_bg_color = """
     <style>
-    body {
-        background-color: #f0f0f0;
+    
+    .container {
         display: flex;
         flex-direction: column;
-        justify-content: center;
         align-items: center;
-        height: 100vh;
+        text-align: center;
+        transform: translate(20%,0)
     }
+
+    .containerx{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        transform: translate(20%,0)
+    }
+
     </style>
     """
     st.markdown(page_bg_color, unsafe_allow_html=True)
 
-    st.title("Samooha")
-    st.subheader("Signup using Snowflake account")
+    
+
+
+    st.markdown("<div class='container'><h1 style='margin: 0'>Samooha</h1></div>", unsafe_allow_html=True)
+
+    st.markdown("<div class='containerx'><h2> Signup using Snowflake account</h2></div>", unsafe_allow_html=True)
+
+   
+
 
     # React Component
     st.components.v1.iframe("http://localhost:5173", height=600, width=1000)
 
+
     # Session state variable to control Signup button focus
     is_button_focused = st.session_state.get("is_button_focused", False)
 
-    # Listen for custom event from React component
-    signup_event = st.session_state.get("signup_event", {})
-    if "args" in signup_event and "detail" in signup_event["args"]:
-        is_button_focused = signup_event["args"]["detail"]
-
-    if is_button_focused:
-        st.markdown('<style>.focused-button { background-color: blue; color: white; }</style>', unsafe_allow_html=True)
-
-    # TOS and Privacy Policy
-    st.text("By signing up to TOI and agreeing to the Privacy Policy.")
-    st.markdown("[Privacy Policy](https://samooha.tech)")
-
-    # Store the updated state in session state
-    st.session_state.is_button_focused = is_button_focused
-
-    # Streamlit Component End
-
+   
 
 if __name__ == "__main__":
     main()
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,75 @@
-import { useState, useEffect } from 'react';
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 const App = () => {
-  const [account, setAccount] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [account, setAccount] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isButtonFocused, setIsButtonFocused] = useState(false);
 
-  useEffect(() => {
-    if (account && username && password) {
-      const event = new CustomEvent('signupButtonFocus', { detail: true });
-      window.dispatchEvent(event);
-    } else {
-      const event = new CustomEvent('signupButtonFocus', { detail: false });
-      window.dispatchEvent(event);
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "account") {
+      setAccount(value);
+    } else if (id === "username") {
+      setUsername(value);
+    } else if (id === "password") {
+      setPassword(value);
     }
-  }, [account, username, password]);
+    setIsButtonFocused(account !== "" && username !== "" && password !== "");
+  };
+
+  const handleSignup = () => {
+    if (account && username && password) {
+      toast.success("Signup successful!");
+    }
+  };
 
   return (
-    <div>
-      <h1>Logo</h1>
-      <h2>Heading</h2>
-      <h3>Subheading</h3>
-
-      <div>
-        <label htmlFor="account">Account</label>
-        <input type="text" id="account" value={account} onChange={(e) => setAccount(e.target.value)} />
+    <div className="card">
+      <div className="inputBox">
+        <input
+          type="text"
+          placeholder="Account Locator"
+          id="account"
+          value={account}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="inputBox">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          value={username}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="inputBox">
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={password}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="toi">
+        By signing up to TOI and agreeing to the{" "}
+        <a href="https://www.samooha.tech/">Privacy Policy</a>.
       </div>
       <div>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <button
+          className={isButtonFocused ? "focused-button" : "nonFocus"}
+          onClick={handleSignup}
+          disabled={!isButtonFocused}
+        >
+          Sign Up
+        </button>
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
+      <ToastContainer />
     </div>
   );
 };
